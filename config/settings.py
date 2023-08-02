3abc8d0d170f5cf646f788ed93bfb526de039820
IlyaKovalchukKIS <mrlivedance@vk.com>
2023-08-02 13:01:07 +0300

    изменены натройки базы данных

--- config/settings.py
+++ config/settings.py
@@ -74,8 +74,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.database_catalog',
+        'USER': 'postgres',
+        'PASSWORD': 537865
     }
 }
 
@@ -100,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
