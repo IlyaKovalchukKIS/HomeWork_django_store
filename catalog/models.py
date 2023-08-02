b86d8a99e0a7850120a714ba295ee2f4d8dadc41
IlyaKovalchukKIS <mrlivedance@vk.com>
2023-08-02 14:16:42 +0300

    написаны модели категорий и продуктов

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='наименование')
+    description = models.CharField(max_length=150, verbose_name='описание')
+    preview = models.ImageField(upload_to='preview/', verbose_name='изображение', **NULLABLE)
+    category = models.CharField(max_length=50, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за покупку')
+    date_creation = models.DateTimeField(verbose_name='дата создания')
+    date_last_change = models.DateTimeField(verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'Название: {self.name} Категория: {self.category}  Цена: {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('price',)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='наименование')
+    description = models.CharField(max_length=150, verbose_name='описание')
+
+
+    def __str__(self):
+        return f'название: {self.name} описание: {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('name',)
