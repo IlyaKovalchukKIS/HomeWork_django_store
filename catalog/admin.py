db53e69e2d1ebfb31c15fc4ab4174b3a738a4a9b
IlyaKovalchukKIS <mrlivedance@vk.com>
2023-08-07 19:09:06 +0300

    добавлено отображение картинки в адмиинке

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,7 +8,7 @@ from catalog.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category',)
+    list_display = ('id', 'name', 'price', 'category', 'preview',)
     search_fields = ('name', 'description',)
     list_filter = ('category',)
 
